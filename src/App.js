3402ed5f3c0adb3cf112043c9812db639ccb510f
alegna19 <angelagomezm@gmail.com>
2023-03-01 19:08:05 +0000

    add video

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from "./components/Header/Header";
+import Video from "./components/Video/Video";
 import "./styles/main.scss";
 
 const App = () => {
   return (
     <div className="App">
       <Header />
+      <Video />
     </div>
   );
 };
---
+++ src/components/Video/Video.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Video.scss";
+
+const Video = () => {
+  return (
+    <div>
+      <video className="video" src="" controls width="100%" height="100%">
+        Video
+      </video>
+    </div>
+  );
+};
+
+export default Video;
